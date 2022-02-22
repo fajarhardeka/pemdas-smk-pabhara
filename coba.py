6c90558e6a5d88ec310445248e72ad72700b0beb
fajarhardeka <fajarhardeka@gmail.com>
2022-02-23 00:05:02 +0700

    update

--- coba.py
+++ coba.py
@@ -1,10 +1,10 @@
 
 penambahan = 1000 + 9360
 pengurangan = 1240 - 501
-perkalian =  210 * 230 - 99 / 2
+campur =  210 * 230 - 99 / 2
 pembagian = 65 / 2
 
 print("1000 + 9360 =", penambahan)
 print("1240 - 501 =", pengurangan)
-print("345 * 43 =", perkalian)
+print("210 * 230 - 99 / 2", campur)
 print("65 / 2=", pembagian)
